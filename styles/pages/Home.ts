765cf935458f0ab05348ddee01783ba0c13fea35
santosfk <santosguilhermefk@gmail.com>
2022-07-02 00:14:50 -0300

    feat:: add background color on home

--- styles/pages/Home.ts
+++ styles/pages/Home.ts
@@ -3,5 +3,5 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100vh;
-  background-color: ${(props) => props.theme.colors.background};
+  background: linear-gradient(180deg, #010030 0%, #7ae8e1 123.83%);
 `;
