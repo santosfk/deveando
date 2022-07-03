c8ad51b8fa105784be104bdd20696a2c7ae12f46
santosfk <santosguilhermefk@gmail.com>
2022-07-03 19:55:58 -0300

    feat:: get prismic documents

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,8 +3,11 @@ import NextHead from "../src/components/nextHead";
 import Image from "next/image";
 import { Container } from "../styles/pages/Home";
 import Header from "../src/components/header/Header";
+import { usePrismicDocuments } from "@prismicio/react";
 
 const Home: NextPage = () => {
+  const [documents] = usePrismicDocuments();
+  console.log(documents);
   return (
     <Container>
       <NextHead title="deveando" />
