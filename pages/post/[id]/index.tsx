b49c36cbeb04dda58340b0420df50542ac9735d9
santosfk <santosguilhermefk@gmail.com>
2022-07-10 21:37:36 -0300

    feat: creating the post page

--- pages/post/[id]/index.tsx
+++ pages/post/[id]/index.tsx
@@ -2,17 +2,23 @@ import React from "react";
 import { useRouter } from "next/router";
 import type { NextPage } from "next";
 import { useSelector } from "react-redux";
+import { Container, HeaderContainer } from "../../../styles/pages/Post";
+import Post from "../../../src/Patterns/post/Post";
+import Header from "../../../src/components/header/Header";
 const OpenPost: NextPage = () => {
   const router = useRouter();
   const { id } = router.query;
-  const { description, img } = useSelector((state: any) => state.blogInfo);
-  console.log(description);
+  const { description, img, title } = useSelector(
+    (state: any) => state.blogInfo
+  );
   return (
-    <div>
-      <h1>{id}</h1>
-      <h2>{description}</h2>
-      <img src={img} alt="" />
-    </div>
+    <Container>
+      <HeaderContainer>
+        <Header />
+      </HeaderContainer>
+      <Post size="bg" title={title} img={img} description="" />
+      <span>{description}</span>
+    </Container>
   );
 };
 export default OpenPost;
