a3b71b5563db2d2f45ce70e3c79a939f291a0109
santosfk <santosguilhermefk@gmail.com>
2022-07-10 22:12:41 -0300

    feat:adding functionality to return home when logo is clicked

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,11 +2,14 @@ import React from "react";
 import header from "../../../public/Header.png";
 import Image from "next/image";
 import { Container, ImgContent } from "./style";
+import Link from "next/link";
 export default function Header() {
   return (
     <Container>
       <ImgContent>
-        <Image src={header} />
+        <Link href="/">
+          <Image src={header} />
+        </Link>
       </ImgContent>
     </Container>
   );
