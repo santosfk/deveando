4758c908057715964da6f66abeb54cfb45df9f3e
santosfk <santosguilhermefk@gmail.com>
2022-08-22 23:39:38 -0300

    feat: add modal props

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,28 +1,41 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import header from "../../../public/Header.png";
 import Image from "next/image";
-import { Container, ImgContent } from "./style";
+import { Container, ImgContent, } from "./style";
 import Link from "next/link";
-import {UserFocus} from "phosphor-react"
+import { UserFocus } from "phosphor-react";
 import { RegisterModal } from "../RegisterModal";
 import { getSession, useSession } from "next-auth/react";
 import { GetServerSideProps } from "next";
 export default function Header() {
-  const [modalIsOpen,setModalIsOpen] = useState(false)
-  const handleModalIsOpen = () =>{
-    setModalIsOpen(!modalIsOpen)
-  }
+  const [modalIsOpen, setModalIsOpen] = useState(false);
+  const handleModalIsOpen = () => {
+    setModalIsOpen(!modalIsOpen);
+  };
+  const variants = {
+    open: { opacity: 1, x: 0 },
+    closed: { opacity: 0, x: "-100vh" },
+  };
 
   return (
     <Container>
-      {modalIsOpen && <RegisterModal setModalIsOpen = {setModalIsOpen}/>}
+    
+
       <ImgContent>
         <Link href="/">
           <Image src={header} />
         </Link>
       </ImgContent>
-          <UserFocus className="user-icon" size={42} weight="fill" onClick={handleModalIsOpen} />
+      <UserFocus
+        className="user-icon"
+        size={42}
+        weight="fill"
+        onClick={handleModalIsOpen}
+      />
+         <RegisterModal
+          setModalIsOpen={setModalIsOpen}
+          modalIsOpen={modalIsOpen}
+        /> 
     </Container>
   );
 }
-
