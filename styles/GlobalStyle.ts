1a219c61f552756353908d97ac7c78fc18cfe4ea
santosfk <santosguilhermefk@gmail.com>
2022-10-12 12:03:45 -0300

    feat: increment time of the gradient animation

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -19,7 +19,7 @@ export default createGlobalStyle`
     background: linear-gradient(358deg, #010030, #52b9b3);
     background-image: linear-gradient(0deg,#010030,#52b9b3,#010030,#52b9b3);
     background-size: 100% 300%;
-    animation: ${backgroundAnimation} 13s ease infinite;
+    animation: ${backgroundAnimation} 20s ease infinite;
     background-repeat:no-repeat;
     font-family: Roboto, sans-serif;
     height: 100%;
