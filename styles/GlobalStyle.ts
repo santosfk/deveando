4a6639050f260103faa6cace8eda45dddb2dbc94
santosfk <santosguilhermefk@gmail.com>
2022-10-12 12:01:42 -0300

    feat: gradient-animation

--- styles/GlobalStyle.ts
+++ styles/GlobalStyle.ts
@@ -1,6 +1,13 @@
 import { createGlobalStyle } from "styled-components";
+import { keyframes } from 'styled-components'
+const backgroundAnimation = keyframes `
+ 
+  0% { background-position: 0% 100%; }
+  100% { background-position: 0% 0%; }
 
+`
 export default createGlobalStyle`
+
   * {
     margin: 0;
     padding: 0;
@@ -8,7 +15,11 @@ export default createGlobalStyle`
   }
   
   body {
-    background: linear-gradient(180deg, #010030 0%, #7ae8e1 );
+
+    background: linear-gradient(358deg, #010030, #52b9b3);
+    background-image: linear-gradient(0deg,#010030,#52b9b3,#010030,#52b9b3);
+    background-size: 100% 300%;
+    animation: ${backgroundAnimation} 13s ease infinite;
     background-repeat:no-repeat;
     font-family: Roboto, sans-serif;
     height: 100%;
