5c01317f278c15636ccb6c1e467ff71c375d7cda
santosfk <santosguilhermefk@gmail.com>
2022-12-04 14:53:19 -0300

    feat: fix home style

--- styles/pages/Home.ts
+++ styles/pages/Home.ts
@@ -3,7 +3,9 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: 100%;
   height: 100%;
-  position: relative;
+
+
+
 
   @media (max-width: 991px) {
     height: 100%;
@@ -11,12 +13,5 @@ export const Container = styled.div`
   @media (max-width: 375px) {
     overflow-x: hidden;
   }
-  .modal-wrapper {
-    left: 50%;
-    top: 30%;
-    position: absolute;
-    transform: translateX(-50%) !important;
-    /* transform: translateY(-50%) !important; */
-    z-index: 10;
-  }
+
 `;
