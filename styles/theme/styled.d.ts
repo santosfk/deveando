e8dcdf0ef9bc2b3be0aee238127db3621bc52081
santosfk <santosguilhermefk@gmail.com>
2023-02-02 23:00:13 -0300

    fix: file name

--- styles/theme/styled.d.ts
+++ styles/theme/styled.d.ts
@@ -1,8 +1,9 @@
 import "styled-components";
+import { DefaultTheme } from "styled-components";
 
-import theme from "./theme";
+import dark from "./dark";
 
-export type Theme = typeof theme;
+export type Theme = typeof dark;
 
 declare module "styled-components" {
   export interface DefaultTheme extends Theme {}
