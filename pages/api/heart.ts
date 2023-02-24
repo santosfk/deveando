731165853870922f314c6be29e10dfbb0242b20d
santosfk <santosguilhermefk@gmail.com>
2023-02-23 23:59:16 -0300

    feat: create update req and fix getHearts types

--- pages/api/heart.ts
+++ pages/api/heart.ts
@@ -1,14 +1,18 @@
 import axios from "axios";
+import { Heart } from "../../src/types/heart";
 const baseUrl = "https://63f6b97c59c944921f786bcb.mockapi.io/hearts";
 const getHearts = async () => {
   try {
-    const data: number = await axios.get(baseUrl);
+    const data: Heart[] = await (await axios.get(baseUrl)).data;
     return data;
   } catch (e) {
     console.log(e);
   }
 };
-const addHeart = async (hearts: number) => {
+const addHeart = async (hearts: Heart) => {
   const data = await axios.post(baseUrl, hearts);
 };
-export { getHearts, addHeart };
+const updateHeart = async (heart: Heart) => {
+  await axios.put(`${baseUrl}/${heart.id}`, heart);
+};
+export { getHearts, addHeart, updateHeart };
